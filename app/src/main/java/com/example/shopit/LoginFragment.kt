5e743306f514061b3f659987a0b8e8077943b43e
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-11-07 15:08:31 +0530

    password validation added

--- app/src/main/java/com/example/shopit/LoginFragment.kt
+++ app/src/main/java/com/example/shopit/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.example.shopit
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,7 +39,28 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+
+        view.next_button.setOnClickListener {
+            if (!isPasswordValid(passwordEditText.text!!)) {
+                passwordTextInput.error = getString(R.string.shr_error_password)
+            } else {
+                // Clear the error.
+                passwordTextInput.error = null
+            }
+        }
+
+        // Clear the error once more than 8 characters are typed.
+        view.passwordEditText.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(passwordEditText.text!!)) {
+                // Clear the error.
+                passwordTextInput.error = null
+            }
+            false
+        }
+
+        return view
     }
 
     companion object {
@@ -57,5 +81,10 @@ class LoginFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
     }
 }
